efbd670fb3e8f6e835d5aef81bd918b1d3cfea07
Oliver Caldwell <ollie@oli.me.uk>
2020-02-10 18:36:37 +0000

    Adding some script docs

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Compiles all Fennel code into Lua assuming you have Aniseed cloned through dep.sh.
+# Usage: deps/aniseed/scripts/compile.sh
+
 nvim -u NONE \
     -c "set rtp+=deps/aniseed" \
     -c "lua require('aniseed.compile').glob('**/*.fnl', 'fnl', 'lua')" \
