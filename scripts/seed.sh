476b41a934924a636716842181ef83da8ff7213f
Oliver Caldwell <ollie@oli.me.uk>
2020-10-08 18:52:02 +0100

    Get seed.sh working for me, guard against tree not existing

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -7,15 +7,18 @@
 # Usage: curl -fL https://raw.githubusercontent.com/Olical/aniseed/master/scripts/seed.sh | sh
 
 mkdir -p scripts
-printf "$(tput bold)Downloading Aniseed Bootstrap script ...\n\n"
+
+printf "$(tput bold)Downloading Aniseed dependency manager script...\n\n"
 curl -fL https://raw.githubusercontent.com/Olical/aniseed/master/scripts/dep.sh -o scripts/dep.sh 2>/dev/null;
 chmod +x scripts/dep.sh
-printf "Cloning Aniseed repo ...\n\n"
-scripts/dep.sh Olical aniseed origin/master 2>/dev/null; 
-printf "Bootstrapping ...\n\n"
+
+printf "Fetching Aniseed...\n\n"
+scripts/dep.sh Olical aniseed origin/master 2>/dev/null;
+
+printf "Creating your new plugin...\n\n"
 cp -r deps/aniseed/seed/* .
 cp deps/aniseed/seed/.gitignore .
-plug_name=${PWD##*/}   
+plug_name=${PWD##*/}
 mv ./fnl/example ./fnl/$plug_name
 mv ./test/fnl/example ./test/fnl/$plug_name
 mv ./plugin/example.vim ./plugin/$plug_name.vim
@@ -23,6 +26,14 @@ sed -i -e "s/example/$plug_name/g" ./test/fnl/$plug_name/main-test.fnl
 sed -i -e "s/example/$plug_name/g" ./fnl/$plug_name/main.fnl
 sed -i -e "s/example/$plug_name/g" ./plugin/$plug_name.vim
 sed -i -e "s/example/$plug_name/g" ./Makefile
-printf "$(tput bold)Running Makefile ...$(tput sgr0)\n\n"
-make -s
-printf "\nDone, $(tput bold)Happy dev.\n\n"; tree  -I 'aniseed'
+
+printf "$(tput bold)Running Makefile...$(tput sgr0)\n\n"
+make -s &
+wait
+
+printf "\nDone, $(tput bold)Happy dev.\n\n"
+
+if command -v tree &> /dev/null
+then
+  tree -I 'aniseed'
+fi
