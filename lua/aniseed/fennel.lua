f3d000436d0b7743a3036be8659551c3307615b1
Oliver Caldwell <ollie@oli.me.uk>
2020-10-10 18:26:11 +0100

    Only persist local-fns if it doesn't error

--- lua/aniseed/fennel.lua
+++ lua/aniseed/fennel.lua
@@ -15,8 +15,17 @@ do
   _0_0 = module_0_
 end
 local function _2_(...)
-  _0_0["aniseed/local-fns"] = {require = {fennel = "aniseed.deps.fennel", nvim = "aniseed.nvim"}}
-  return {require("aniseed.deps.fennel"), require("aniseed.nvim")}
+  local ok_3f_0_, val_0_ = nil, nil
+  local function _2_()
+    return {require("aniseed.deps.fennel"), require("aniseed.nvim")}
+  end
+  ok_3f_0_, val_0_ = pcall(_2_)
+  if ok_3f_0_ then
+    _0_0["aniseed/local-fns"] = {require = {fennel = "aniseed.deps.fennel", nvim = "aniseed.nvim"}}
+    return val_0_
+  else
+    return print(val_0_)
+  end
 end
 local _1_ = _2_(...)
 local fennel = _1_[1]
