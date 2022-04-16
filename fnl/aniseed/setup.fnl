05fea5df034d82cd10154eae4ddafc0629db71d5
Oliver Caldwell <ollie@oli.me.uk>
2022-04-16 12:38:58 +0100

    Add commands to evaluate Fennel without Conjure (although I still recommend you use Conjure!) fixes #111

--- fnl/aniseed/setup.fnl
+++ fnl/aniseed/setup.fnl
@@ -1,7 +1,31 @@
 (module aniseed.setup
   {autoload {nvim aniseed.nvim
+             a aniseed.core
+             eval aniseed.eval
              env aniseed.env}})
 
 (defn init []
+  (nvim.create_user_command
+    :AniseedEval
+    (fn [cmd]
+      (let [(ok? res) (eval.str cmd.args {})]
+        (if ok?
+          (nvim.echo res)
+          (nvim.err_writeln res))))
+    {:nargs 1})
+
+  (nvim.create_user_command
+    :AniseedEvalFile
+    (fn [cmd]
+      (let [code (a.slurp cmd.args)]
+        (if code
+          (let [(ok? res) (eval.str code {})]
+            (if ok?
+              (nvim.echo res)
+              (nvim.err_writeln res)))
+          (nvim.err_writeln (.. "File '" (or cmd.args "nil") "' not found")))))
+    {:nargs :?
+     :complete :file})
+
   (when nvim.g.aniseed#env
     (env.init nvim.g.aniseed#env)))
--- lua/aniseed/setup.lua
+++ lua/aniseed/setup.lua
@@ -11,10 +11,35 @@ do
   _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
 end
 local autoload = (require("aniseed.autoload")).autoload
-local env, nvim = autoload("aniseed.env"), autoload("aniseed.nvim")
-do end (_2amodule_locals_2a)["env"] = env
+local a, env, eval, nvim = autoload("aniseed.core"), autoload("aniseed.env"), autoload("aniseed.eval"), autoload("aniseed.nvim")
+do end (_2amodule_locals_2a)["a"] = a
+_2amodule_locals_2a["env"] = env
+_2amodule_locals_2a["eval"] = eval
 _2amodule_locals_2a["nvim"] = nvim
 local function init()
+  local function _1_(cmd)
+    local ok_3f, res = eval.str(cmd.args, {})
+    if ok_3f then
+      return nvim.echo(res)
+    else
+      return nvim.err_writeln(res)
+    end
+  end
+  nvim.create_user_command("AniseedEval", _1_, {nargs = 1})
+  local function _3_(cmd)
+    local code = a.slurp(cmd.args)
+    if code then
+      local ok_3f, res = eval.str(code, {})
+      if ok_3f then
+        return nvim.echo(res)
+      else
+        return nvim.err_writeln(res)
+      end
+    else
+      return nvim.err_writeln(("File '" .. (cmd.args or "nil") .. "' not found"))
+    end
+  end
+  nvim.create_user_command("AniseedEvalFile", _3_, {nargs = "?", complete = "file"})
   if nvim.g["aniseed#env"] then
     return env.init(nvim.g["aniseed#env"])
   else
