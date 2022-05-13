b239425df2fee031c4f0d0bdb0d1ad69feaa37f0
Oliver Caldwell <ollie@oli.me.uk>
2022-05-13 20:14:40 +0100

    Support relying on embedded macro files #117

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,9 +2,16 @@
 
 # Compiles all Fennel code into Lua assuming you have Aniseed cloned through dep.sh.
 # Usage: deps/aniseed/scripts/compile.sh
+# Usage: ANISEED_EMBED_PREFIX=my-plugin-name deps/aniseed/scripts/compile.sh
 
 nvim --headless -u NONE \
     -c "let &runtimepath = &runtimepath . ',deps/aniseed,' . getcwd()" \
     -c "lua package.path = package.path .. ';$(pwd)/lua/?.lua;deps/aniseed/lua/?.lua'" \
     -c "lua require('aniseed.compile').glob('**/*.fnl', 'fnl', 'lua')" \
     +q
+
+if [ -n "$ANISEED_EMBED_PREFIX" ]; then
+    find "lua/$ANISEED_EMBED_PREFIX" -type f -name "*.lua" -exec sed -i.aniseed_bak "s/\"aniseed\./\"$ANISEED_EMBED_PREFIX.aniseed./g" {} \;
+    find "lua/$ANISEED_EMBED_PREFIX" -type f -name "*.fnl" -exec sed -i.aniseed_bak "s/\"aniseed\./\"$ANISEED_EMBED_PREFIX.aniseed./g" {} \;
+    find . -type f -name '*.aniseed_bak' -delete
+fi
